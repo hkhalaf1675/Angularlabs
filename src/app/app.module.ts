64c22a8cde7c623c7ef0ddd8c2953059696e30f8
Hassan Tawfik <h.khalaf16.75@gmail.com>
2023-10-24 20:08:06 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { ProductsComponent } from './Components/products/products.component';
 import { ProductcardComponent } from './Components/productcard/productcard.component';
 import { SliderComponent } from './Components/slider/slider.component';
+import { ImageshowComponent } from './Components/imageshow/imageshow.component';
+import { TodolistComponent } from './Components/todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,14 @@ import { SliderComponent } from './Components/slider/slider.component';
     FooterComponent,
     ProductsComponent,
     ProductcardComponent,
-    SliderComponent
+    SliderComponent,
+    ImageshowComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
